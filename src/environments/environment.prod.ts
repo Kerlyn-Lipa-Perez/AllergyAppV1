e453f0038e31b5758ad51e73bad24005d21907b4
KerlynLipaPerez <kerlyn.lipa@ucsm.edu.pe>
2023-04-19 21:21:51 -0500

    primer avance

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyDauAqvdt_6quuQ589W-UpIi8PTrSpFpLA",
+    authDomain: "allergy-app2.firebaseapp.com",
+    projectId: "allergy-app2",
+    storageBucket: "allergy-app2.appspot.com",
+    messagingSenderId: "1092011584449",
+    appId: "1:1092011584449:web:3f02d4e5670c0255c79d40"
+  }
+};
